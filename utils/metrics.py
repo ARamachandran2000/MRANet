d876397ca0ca139be4d08d4cd577f64a418caf73
Sravan Chittupalli <sravanchittupalli7@gmail.com>
2021-12-12 16:22:21 +0530

    Added metrics code

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,12 @@
 import numpy as np
+import os
 import cv2
+
+# Change this to gt directory
+gt_file = './gt/Final_Labels_Final_2/'
+# change this to predictions directory
+pred_file = './pred_mra/'
+
 
 EPS = 1e-12
 
@@ -26,16 +33,21 @@ def get_iou_dice(gt, pr, n_classes):
     gt = change_image_format(gt)
     pr = change_image_format(pr)
 
-    class_wise_iou = np.zeros(n_classes)
-    class_wise_dice = np.zeros(n_classes)
+    class_wise_iou = np.ones(n_classes)
+    class_wise_dice = np.ones(n_classes)
     for cl in range(n_classes):
         intersection = np.sum((gt == cl)*(pr == cl))
         union = np.sum(np.maximum((gt == cl), (pr == cl)))
         # mask_sum = np.sum(gt == cl) + np.sum(pr == cl)
         iou = float(intersection)/(union + EPS)
-        dice = ((2*float(intersection))/(union + intersection) + EPS)
+        dice = ((2*float(intersection))/((union + intersection) + EPS))
         class_wise_iou[cl] = iou
         class_wise_dice[cl] = dice
+
+    where_0 = np.where(class_wise_iou == 0)
+    class_wise_iou[where_0] = 1
+    where_0 = np.where(class_wise_dice == 0)
+    class_wise_dice[where_0] = 1
     return class_wise_iou, class_wise_dice
 
 def get_pixel_accuracy(gt, pr, n_classes):
@@ -78,14 +90,24 @@ def recall(gt, pr, n_classes):
     return class_wise_recall
 
 
-gt_file = './gt/Final_Labels_Final_2/'
-pred_file = './pred_vggnet/'
+total_iou = 0
+total_dice = 0
+total_pixacc = 0
+total_recall = 0
+total_prec = 0
 
-filename = '11223.png'
-gt = cv2.imread(gt_file+filename)
-pr = cv2.imread(pred_file+filename)
+no_files = len(os.listdir(pred_file))
+for f in os.listdir(pred_file):
 
-print(get_iou_dice(gt, pr, 4))
-print(get_pixel_accuracy(gt, pr, 4)*100)
-print(precision(gt, pr, 4))
-print(recall(gt, pr, 4))
+    filename = f
+    gt = cv2.imread(gt_file+filename)
+    pr = cv2.imread(pred_file+filename)
+
+    iou, dice = get_iou_dice(gt, pr, 4)
+    total_iou += iou
+    total_dice += dice
+    total_pixacc += get_pixel_accuracy(gt, pr, 4)
+    total_recall += recall(gt, pr, 4)
+    total_prec += precision(gt, pr, 4)
+
+print(np.average(total_iou/no_files), np.average(total_dice/no_files), total_pixacc/no_files, np.average(total_recall/no_files), np.average(total_prec/no_files))
